733272948c16b4bbf502044f933a54cff609b239
Dan <danielcrw97@gmail.com>
2018-08-26 23:20:49 +0100

    new light stuff

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -7,12 +7,14 @@ public class Torch : MonoBehaviour {
     private bool flipped;
     private Light torch;
     private SpriteRenderer rendererComp;
+    public float energyRemaining;
 
     void Awake()
     {
         rendererComp = GetComponent<SpriteRenderer>();
         torch = GetComponentInChildren<Light>();
         flipped = rendererComp.flipX;
+        energyRemaining = 1000f;
     }
 
     void Start () {
@@ -27,5 +29,12 @@ public class Torch : MonoBehaviour {
             torch.gameObject.transform.localPosition = new Vector3(-torchPos.x, -torchPos.y, torchPos.z);
             flipped = rendererComp.flipX;
         }
+
+        energyRemaining = energyRemaining - Time.deltaTime;
 	}
+
+    public void SetPosition(Vector2 localPos)
+    {
+        torch.gameObject.transform.localPosition = new Vector3(localPos.x, localPos.y, torch.gameObject.transform.localPosition.z);
+    }
 }

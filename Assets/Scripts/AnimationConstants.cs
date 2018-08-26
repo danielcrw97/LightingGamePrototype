01b15c343ab10b0b1453f12f9b71e774ced1eaa0
Dan <danielcrw97@gmail.com>
2018-08-26 17:17:53 +0100

    refactored

---
+++ Assets/Scripts/AnimationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public static class AnimationConstants
+{
+    public const string PLAYER_RUN = "Running";
+    public const string PLAYER_JUMP = "Jumping";
+    public const string PLAYER_FALL = "Falling";
+}
+
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,21 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Generic character controller for a 2d sprite that can run and jump.
+ */
 public class Controller : MonoBehaviour
 {
 
-    // Fields 
+    //TODO replace all magic numbers
+    //TODO do attacking in seperate component.
 
     public float speed;
     public float jumpSpeed;
     public float fallingGravityMult;
     public float notHoldingJumpMult;
-    public float terminalVelocity;
 
     private Rigidbody2D rb;
-    private Collider2D collider;
+    private Collider2D colliderComp;
     private Animator animator;
-    private SpriteRenderer renderer;
+    private SpriteRenderer rendererComp;
     private bool walkingRight;
     private bool isJumping;
 
@@ -27,11 +30,10 @@ public class Controller : MonoBehaviour
         this.jumpSpeed = 7f;
         this.fallingGravityMult = 1.6f;
         this.notHoldingJumpMult = 2f;
-        this.terminalVelocity = 5f;
         this.rb = gameObject.GetComponent<Rigidbody2D>();
-        this.collider = gameObject.GetComponent<Collider2D>();
+        this.colliderComp = gameObject.GetComponent<Collider2D>();
         this.animator = gameObject.GetComponent<Animator>();
-        this.renderer = gameObject.GetComponent<SpriteRenderer>();
+        this.rendererComp = gameObject.GetComponent<SpriteRenderer>();
         this.isJumping = false;
     }
 
@@ -43,38 +45,31 @@ public class Controller : MonoBehaviour
     void FixedUpdate()
     {
 
-        float horMovement = Input.GetAxis("Horizontal") * speed;
-        if(horMovement < 0f)
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        if(rb.velocity.x < 0f)
         {
-            renderer.flipX = true;
+            rendererComp.flipX = true;
         }
-        else if(horMovement > 0f)
+        else if(rb.velocity.x > 0f)
         {
-            renderer.flipX = false;
+            rendererComp.flipX = false;
         }
 
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
         {
-            animator.SetBool("Running", true);
+            animator.SetBool(AnimationConstants.PLAYER_RUN, true);
         }
         else
         {
-            animator.SetBool("Running", false);
+            animator.SetBool(AnimationConstants.PLAYER_RUN, false);
         }
-        animator.SetFloat("Speed", Mathf.Abs(horMovement));
 
         float verMovement = rb.velocity.y;
-
-        if (!isJumping && Input.GetKey(KeyCode.LeftShift))
-        {
-            horMovement = horMovement * 1.75f;
-        }
-
 
         if (!isJumping && Input.GetKey(KeyCode.Space))
         {
             isJumping = true;
-            verMovement = jumpSpeed;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
 
         if(isJumping)
@@ -82,28 +77,8 @@ public class Controller : MonoBehaviour
             HandleJump();
         }
 
-        /*
-        if (isJumping && rb.velocity.y < 0f)
-        {
-            verMovement = rb.velocity.y + (Physics2D.gravity.y * (fallingGravityMult - 1) * Time.fixedDeltaTime);
-        }
-
-        
-        if (isJumping && rb.velocity.y > 0f && !Input.GetKey(KeyCode.Space))
-        {
-            verMovement = rb.velocity.y  + (Physics2D.gravity.y * (notHoldingJumpMult - 1) * Time.fixedDeltaTime);
-        }
-
-        if (isJumping)
-        {
-            CheckIfCurrentlyJumping();
-        }
-        */
-
-        animator.SetBool("Jumping", isJumping);
-        animator.SetBool("Falling", isJumping && (rb.velocity.y < 0));
-
-        rb.velocity = new Vector2(horMovement, verMovement);
+        animator.SetBool(AnimationConstants.PLAYER_JUMP, isJumping);
+        animator.SetBool(AnimationConstants.PLAYER_FALL, isJumping && (rb.velocity.y < 0));
     }
 
     private void HandleJump()
@@ -126,17 +101,20 @@ public class Controller : MonoBehaviour
     {
         if (isJumping && rb.velocity.y < 0f)
         {
-            Vector2 midBottom = new Vector2(collider.bounds.center.x, collider.bounds.min.y - 0.1f);
+            Vector2 midBottom = new Vector2(colliderComp.bounds.center.x, colliderComp.bounds.min.y - 0.1f);
             RaycastHit2D rayHit = Physics2D.Raycast(midBottom, Vector2.down);
-            if (rayHit != null && rayHit.distance < 0.1f)
+            if (rayHit != null && rayHit.distance < 0.3f)
             {
                 isJumping = false;
+                animator.SetBool(AnimationConstants.PLAYER_JUMP, false);
+                animator.SetBool(AnimationConstants.PLAYER_FALL, false);
             }
         }
     }
 
     private float Limit(float speed)
     {
+        float terminalVelocity = 5.0f;
         return Mathf.Clamp(speed, -terminalVelocity, terminalVelocity);
     }
 }

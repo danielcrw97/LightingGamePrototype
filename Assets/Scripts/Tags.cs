c0b5f27dc3e51f1bf64cf1ba3c85e98d016fbb62
Dan <danielcrw97@gmail.com>
2018-08-27 11:14:12 +0100

    Torch code

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public static class Tags
+{
+    public const String CRYSTAL_TAG = "Crystal";
+}
+
--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -7,19 +7,26 @@ public class Torch : MonoBehaviour {
     private bool flipped;
     private Light torch;
     private SpriteRenderer rendererComp;
+    private List<Transform> crystalPositions;
     public float energyRemaining;
+    public float rechargeRate;
 
     void Awake()
     {
-        rendererComp = GetComponent<SpriteRenderer>();
-        torch = GetComponentInChildren<Light>();
-        flipped = rendererComp.flipX;
-        energyRemaining = 1000f;
+        this.torch = GetComponentInChildren<Light>();
+        List<GameObject> crystals = new List<GameObject>(GameObject.FindGameObjectsWithTag(Tags.CRYSTAL_TAG));
+        foreach(GameObject crystal in crystals)
+        {
+            crystalPositions.Add(crystal.transform);
+        }
     }
 
     void Start () {
-		
-	}
+        this.rendererComp = GetComponent<SpriteRenderer>();
+        this.flipped = rendererComp.flipX;
+        this.energyRemaining = 1000f;
+        this.rechargeRate = 200f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,10 +38,50 @@ public class Torch : MonoBehaviour {
         }
 
         energyRemaining = energyRemaining - Time.deltaTime;
+
+        if(energyRemaining <= 0f)
+        {
+            // Die
+        }
+
+        foreach(Transform crystalPos in crystalPositions)
+        {
+            // If close enough to a crystal listen for recharging inputs. 
+            if((crystalPos.position - transform.position).magnitude < 1f)
+            {
+
+            }
+        }
 	}
+
+    public void SapEnergy(float amount)
+    {
+        energyRemaining = energyRemaining - amount;
+    }
 
     public void SetPosition(Vector2 localPos)
     {
         torch.gameObject.transform.localPosition = new Vector3(localPos.x, localPos.y, torch.gameObject.transform.localPosition.z);
+    }
+
+    public void Recharge()
+    {
+
+    }
+
+    IEnumerator RechargeCoroutine(Transform target)
+    {
+        while (Vector3.Distance(transform.position, target.position) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, smoothing * Time.deltaTime);
+
+            yield return null;
+        }
+
+        print("Reached the target.");
+
+        yield return new WaitForSeconds(3f);
+
+        print("MyCoroutine is now finished.");
     }
 }

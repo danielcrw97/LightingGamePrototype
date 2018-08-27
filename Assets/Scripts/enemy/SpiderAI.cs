8a3ccf7fb31715a10c23ea7da97145f878593e21
Dan <danielcrw97@gmail.com>
2018-08-27 23:59:22 +0100

    Big AI changes

--- Assets/Scripts/enemy/SpiderAI.cs
+++ Assets/Scripts/enemy/SpiderAI.cs
@@ -5,43 +5,150 @@ using UnityEngine;
 
 public class SpiderAI : MonoBehaviour {
 
-    [SerializeField]
-    private GameObject waypoint1;
+    private enum State
+    {
+        IDLE,
+        PATROLLING,
+        CHASING,
+        ATTACKING,
+        IMMOBILIZED,
+    }
 
     [SerializeField]
-    private GameObject waypoint2;
+    private GameObject rightBarrier;
+
+    [SerializeField]
+    private GameObject leftBarrier;
 
     public float speed;
+    public float health;
+
+    private SpriteRenderer rendererComp;
     private Rigidbody2D rb;
     private Transform target;
     private Animator animator;
+    private State state;
+    private bool facingRight;
 
+    private const float ATTACK_DISTANCE = 1.3f;
+    private const float SPIDER_RANGE = 1.2f;
 
-    private void Start() {
-        this.animator = gameObject.GetComponent<Animator>();
+    void Awake() {
         this.target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.rendererComp = GetComponent<SpriteRenderer>();
+        this.state = State.PATROLLING;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //follow player
-        if (Vector2.Distance(transform.position, target.position) > 1.5f)
+    }
+
+    void FixedUpdate()
+    {
+        this.facingRight = !rendererComp.flipX;
+
+        switch(state)
         {
-            animator.SetBool("spiderAttacking", false);
-            animator.SetBool("spiderWalking", true);
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            case State.PATROLLING:
+                Patrol();
+                break;
+
+            case State.ATTACKING:
+                Attack();
+                break;
+
+            case State.CHASING:
+                Chase();
+                break;
+
+            case State.IMMOBILIZED:
+                Immobilize();
+                break;
+
+            case State.IDLE:
+                break;
+
+            default:
+                Patrol();
+                break;
         }
-        //Attack on player
+
+    }
+
+    public void Move(bool moveRight)
+    {
+        if (moveRight)
+        {
+            this.rb.velocity = Vector2.right * speed * Time.fixedDeltaTime;
+        }
         else
         {
-            animator.SetBool("spiderAttacking", true);
-            Debug.Log("attack on");
+            this.rb.velocity = Vector2.left * speed * Time.fixedDeltaTime;
         }
     }
 
-    public void Move(Vector2 targetPos)
+    private void Patrol()
     {
-        
+        float distance;
+        if(facingRight)
+        {
+            distance = rightBarrier.transform.position.x - transform.position.x;
+        }
+        else
+        {
+            distance = transform.position.x - leftBarrier.transform.position.x;
+        }
+        if(distance < 0.05f)
+        {
+
+        }
+        Move(facingRight);
+    }
+
+    private void Chase()
+    {
+
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Immobilize()
+    {
+
+    }
+
+    private void CheckForLedge()
+    {
+
+    }
+
+    public void SwitchDirection()
+    {
+
+    }
+
+    private bool canTargetPlayer()
+    {
+        // If the spider can sense the players and he is not beyond the spiders limits - go for it!
+        bool canSense = ((target.transform.position - transform.position).magnitude) < SPIDER_RANGE;
+        bool isBeyondLimits = (target.transform.position.x < leftBarrier.transform.position.x) ||
+            (target.transform.position.x > rightBarrier.transform.position.x);
+        return canSense && !isBeyondLimits;
+    }
+
+    private IEnumerator WaitThenTurnAround(float seconds)
+    {
+        state = State.IDLE;
+        yield return new WaitForSeconds(seconds);
+        state = State.PATROLLING;
     }
 }

1d64876cc7292c9c034e0089131b1c0192513cd0
ankix <djankix@gamil.com>
2018-08-28 19:54:52 +0530

    Player Heath_System with Ui and Damage

---
+++ Assets/HealthSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour {
+    [SerializeField]
+    private Sprite[] heart;
+    public Image HUI;
+
+    public int player_health = 3;
+    public int damage = 1;
+
+    void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Damage();
+            HUI.sprite = heart[player_health]; 
+        }
+    }
+    public void Damage()
+    {
+            player_health -= damage;
+    }
+}

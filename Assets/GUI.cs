d009f03b4a12ef13ac1ad7ac4ac4fb167a1d610c
ankix <djankix@gamil.com>
2018-08-29 00:36:04 +0530

    Player HealthSystem scrpit

---
+++ Assets/GUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI : MonoBehaviour {
+
+    public Transform SettingMenu;
+	void Start () {
+        SettingMenu.gameObject.GetComponent<Transform>();
+	}
+	
+	void Update () {
+		
+	}
+    public On
+}

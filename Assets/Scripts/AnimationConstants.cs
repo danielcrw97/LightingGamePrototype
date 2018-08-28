b46564ea7dcd974678d20c9ef65b60e8079036ab
ankix <djankix@gamil.com>
2018-08-28 17:44:25 +0530

    player attacks basic

--- Assets/Scripts/AnimationConstants.cs
+++ Assets/Scripts/AnimationConstants.cs
@@ -6,9 +6,14 @@ using System.Text;
 
 public static class AnimationConstants
 {
+    //player Aniamtions
     public const string PLAYER_RUN = "Running";
     public const string PLAYER_JUMP = "Jumping";
     public const string PLAYER_FALL = "Falling";
+    public const string PLAYER_CONE_ATTACK = "PConeAttack";
+    public const string PLAYER_CIRCLULAR_ATTACK = "PCircularAttack";
+
+    //spider Animations
     public const string SPIDER_ATTACK = "Attacking";
 }
 
--- Assets/Scripts/Attacks.cs
+++ Assets/Scripts/Attacks.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Attacks : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    private Animator animator;
+ 
+    void Start () {
+        this.animator = gameObject.GetComponent<Animator>();
+    }	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        P_Cone_Attack();
+        P_Circlular_Attack();
+    }
+    private void P_Cone_Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger(AnimationConstants.PLAYER_CONE_ATTACK);
+        }
+    }
+    private void P_Circlular_Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger(AnimationConstants.PLAYER_CIRCLULAR_ATTACK); 
+        }
+
+    }
 }

018db7be43448a0af692ba29e80cf12f869c9824
ankix <djankix@gamil.com>
2018-08-29 19:25:47 +0530

    Button Click SoundEffect Added

--- Assets/GUI.cs
+++ Assets/GUI.cs
@@ -8,35 +8,47 @@ public class GUI : MonoBehaviour
     private Animator animator;
     [SerializeField]
     private Slider fx, music;
-    private bool quit;
+    private AudioSource audiosource;
+
     void Start()
     {
         this.animator = gameObject.GetComponent<Animator>();
+        this.audiosource = gameObject.GetComponent<AudioSource>();
     }
- 
+
     public void Setting_Btn()
     {
         animator.SetBool(AnimationConstants.SETTING_BTN_CLICKED, true);
+            playaudio();
     }
     public void Setting_Back_Btn()
     {
         animator.SetBool(AnimationConstants.SETTING_BTN_CLICKED, false);
+            playaudio();
     }
 
     //Quite Application
     public void Quit()
     {
         animator.SetBool(AnimationConstants.APPLICATION_WARN, true);
+            playaudio();
     }
 
     //warning YES or NO
     public void warningforYes()
     {
         Application.Quit();
+            playaudio();
     }
     public void warningforNo()
     {
         animator.SetBool(AnimationConstants.APPLICATION_WARN, false);
+            playaudio();
+    }
+
+    private void playaudio()
+    {
+        audiosource.Play();
     }
 }
 

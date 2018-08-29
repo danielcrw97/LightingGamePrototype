b689d952f914c45edf00d7388d4e08cdb59d2f9f
ankix <djankix@gamil.com>
2018-08-30 00:04:45 +0530

    Controller scripet now connected to the health_system for getting player health ref.

--- Assets/Scripts/player/HealthSystem.cs
+++ Assets/Scripts/player/HealthSystem.cs
@@ -3,23 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthSystem : MonoBehaviour {
+public class HealthSystem : Controller{
     [SerializeField]
     private Sprite[] heart;
     public Image HUI;
 
-    public int player_health = 3;
-    public int damage = 1;
 
     void FixedUpdate() {
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            Damage();
-            HUI.sprite = heart[player_health]; 
-        }
-    }
-    public void Damage()
-    {
-            player_health -= damage;
+            HUI.sprite = heart[health];
     }
 }

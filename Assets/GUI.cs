7eed8e3b8aa32f77602a185e5c64c5cda4e69383
ankix <djankix@gamil.com>
2018-08-29 18:32:04 +0530

    Game GUI

--- Assets/GUI.cs
+++ Assets/GUI.cs
@@ -1,15 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GUI : MonoBehaviour {
+public class GUI : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField]
+    private Slider fx, music;
+    private bool quit;
+    void Start()
+    {
+        this.animator = gameObject.GetComponent<Animator>();
+    }
+ 
+    public void Setting_Btn()
+    {
+        animator.SetBool(AnimationConstants.SETTING_BTN_CLICKED, true);
+    }
+    public void Setting_Back_Btn()
+    {
+        animator.SetBool(AnimationConstants.SETTING_BTN_CLICKED, false);
+    }
 
-    public Transform SettingMenu;
-	void Start () {
-        SettingMenu.gameObject.GetComponent<Transform>();
-	}
-	
-	void Update () {
-		
-	}
+    //Quite Application
+    public void Quit()
+    {
+        animator.SetBool(AnimationConstants.APPLICATION_WARN, true);
+    }
+
+    //warning YES or NO
+    public void warningforYes()
+    {
+        Application.Quit();
+    }
+    public void warningforNo()
+    {
+        animator.SetBool(AnimationConstants.APPLICATION_WARN, false);
+    }
 }
+
+

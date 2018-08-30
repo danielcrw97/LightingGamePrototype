8dc26f638cca3f43eeef9e8ffa5d0aea487aaf0a
Dan <danielcrw97@gmail.com>
2018-08-30 21:56:14 +0100

    Scene transitions

---
+++ Assets/FadeToNextScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeToNextScene : MonoBehaviour {
+
+    [SerializeField]
+    private string nextScene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
---
+++ Assets/SceneTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour {
+
+    private Animator fader;
+
+    void Awake()
+    {
+        fader = GameObject.Find("BlackFade").GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == Tags.PLAYER_TAG)
+        {
+            fader.SetTrigger("FadeOut");
+        }
+    }
+}
